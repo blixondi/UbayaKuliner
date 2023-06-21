fd01aff60a56d9569782cc2b0570c9f061d5e61c
blixondi <blixondi@gmail.com>
2023-06-21 17:31:18 +0700

    ui register fragment + user model for LiveData done

--- app/src/main/java/com/shem/ubayafood/model/Model.kt
+++ app/src/main/java/com/shem/ubayafood/model/Model.kt
@@ -3,12 +3,29 @@ package com.shem.ubayafood.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
+
+//model for Room database
 @Entity
-data class User(
+data class Session(
     @ColumnInfo(name="username")
     var username: String,
 ){
     @PrimaryKey(autoGenerate = false)
     var id:Int = 0
 }
+
+//model for Live Data
+data class User(
+    @SerializedName("id")
+    val user_id:Int?,
+    val username: String?,
+    @SerializedName("first_name")
+    val firstName: String?,
+    @SerializedName("last_name")
+    val lastName: String?,
+    val password: String?
+)
+
+
