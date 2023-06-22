a4786ece9b35e95a4635f5c0af1e98f91ee7c87a
blixondi <blixondi@gmail.com>
2023-06-22 10:11:16 +0700

    tweak stuff

--- app/src/main/java/com/shem/ubayafood/view/LoginFragment.kt
+++ app/src/main/java/com/shem/ubayafood/view/LoginFragment.kt
@@ -5,16 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.google.android.material.textfield.TextInputEditText
 import com.shem.ubayafood.R
+import com.shem.ubayafood.viewmodel.UserViewModel
 
 class LoginFragment : Fragment() {
-
-
+    private lateinit var viewModel: UserViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -23,5 +24,12 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val txtUsername = view.findViewById<TextInputEditText>(R.id.txtUsername)
+        val txtPassword = view.findViewById<TextInputEditText>(R.id.txtPassword)
+        val btnLogin = view.findViewById<Button>(R.id.btnLogin)
+
+    }
 
 }
---
+++ app/src/main/java/com/shem/ubayafood/viewmodel/UserViewModel.kt
@@ -0,0 +1,70 @@
+package com.shem.ubayafood.viewmodel
+
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
+import com.shem.ubayafood.model.User
+
+class UserViewModel(Application: Application): AndroidViewModel(Application) {
+    val userLD = MutableLiveData<User>()
+    val statusLD = MutableLiveData<String>()
+    val TAG = "volleytag"
+    private var queue: RequestQueue? = null
+
+
+    fun login(username: String, password: String){
+        queue = Volley.newRequestQueue(getApplication())
+        val url = "https://kenhosting.ddns.net/uas-anmp/user/login.php"
+        val stringRequest = object : StringRequest(
+            Request.Method.POST, url,
+            {
+                val result = Gson().fromJson<User>(it, User::class.java)
+                userLD.value = result
+                Log.d("showvoley", result.toString())
+            },
+            {
+                Log.d("showvoley", it.toString())
+            }) {
+            override fun getParams(): MutableMap<String, String> {
+                val map = HashMap<String, String>()
+                map.set("username", username)
+                map.set("password", password)
+                return map
+            }
+        }
+        stringRequest.tag = TAG
+        queue?.add(stringRequest)
+    }
+
+    fun register(username: String, firstName: String, lastName: String, password: String){
+        queue = Volley.newRequestQueue(getApplication())
+        val url = "https://kenhosting.ddns.net/uas-anmp/user/register.php"
+        val stringRequest = object : StringRequest(
+            Request.Method.POST, url,
+            {
+                val result = Gson().fromJson<User>(it, User::class.java)
+                userLD.value = result
+                Log.d("showvoley", result.toString())
+            },
+            {
+                Log.d("showvoley", it.toString())
+            }) {
+            override fun getParams(): MutableMap<String, String> {
+                val map = HashMap<String, String>()
+                map.set("username", username)
+                map.set("password", password)
+                map.set("first_name",firstName)
+                map.set("last_name",lastName)
+                return map
+            }
+        }
+        stringRequest.tag = TAG
+        queue?.add(stringRequest)
+    }
+}
