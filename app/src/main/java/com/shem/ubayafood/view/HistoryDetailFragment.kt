e50ca749ead96a5a34d0166f21b94ebf395cdfe4
blixondi <blixondi@gmail.com>
2023-06-22 13:10:40 +0700

    change color on buttons

--- app/src/main/java/com/shem/ubayafood/view/HistoryDetailFragment.kt
+++ app/src/main/java/com/shem/ubayafood/view/HistoryDetailFragment.kt
@@ -8,14 +8,15 @@ import android.view.ViewGroup
 import com.shem.ubayafood.R
 
 
-class MenuFragment : Fragment() {
+class HistoryDetailFragment : Fragment() {
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
+        return inflater.inflate(R.layout.fragment_history_detail, container, false)
     }
 
 
